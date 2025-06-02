444d746b623739b26f410977324c5f9b25aa8f25
khyati-07 <141446282+khyati-07@users.noreply.github.com>
2025-06-02 18:09:08 +0530

    Update gestureRecognitionGUI.m

--- Hand-Gesture-MATLAB/gui/gestureRecognitionGUI.m
+++ Hand-Gesture-MATLAB/gui/gestureRecognitionGUI.m
@@ -1,30 +1,56 @@
-% gestureRecognitionGUI.m
-% GUI for EMG Gesture Recognition
 
 function gestureRecognitionGUI()
-    % Create GUI figure
-    f = figure('Name', 'EMG Gesture Recognition', 'NumberTitle', 'off', ...
-               'Position', [500, 300, 400, 300]);
-    
-    % Start Camera Button
-    uicontrol(f, 'Style', 'pushbutton', 'String', 'Start Camera', ...
-              'Position', [150, 200, 100, 40], 'Callback', @startCamera);
-    
+    fig = figure('Name', 'EMG Gesture Recognition', 'Position', [500 300 400 300]);
+
+    % Train Model Button
+    uicontrol('Style', 'pushbutton', 'String', 'Train Model', ...
+        'Position', [150 220 100 30], 'Callback', @trainCallback);
+
+    % Load Sample Button
+    uicontrol('Style', 'pushbutton', 'String', 'Load Sample', ...
+        'Position', [150 170 100 30], 'Callback', @loadSampleCallback);
+
+    % Predict Button
+    uicontrol('Style', 'pushbutton', 'String', 'Predict', ...
+        'Position', [150 120 100 30], 'Callback', @predictCallback);
+
     % Exit Button
-    uicontrol(f, 'Style', 'pushbutton', 'String', 'Exit', ...
-              'Position', [150, 140, 100, 40], 'Callback', 'close(gcf)');
-    
-    % Callback function for Start Camera
-    function startCamera(~, ~)
-        % Initialize webcam
-        cam = webcam;
-        h = figure;
-        while ishandle(h)
-            img = snapshot(cam);
-            imshow(img);
-            title('Live Camera - Press Ctrl+C to stop');
-            drawnow;
-        end
-        clear cam;
+    uicontrol('Style', 'pushbutton', 'String', 'Exit', ...
+        'Position', [150 70 100 30], 'Callback', @(~,~) close(fig));
+
+    % Prediction Display
+    uicontrol('Style', 'text', 'String', 'Prediction: None', ...
+        'Position', [100 30 200 20], 'Tag', 'predictionLabel');
+
+    assignin('base', 'currentSample', []);
+end
+
+
+
+function trainCallback(~, ~)
+    train_model();  % Calls your function
+    msgbox('Model trained and saved to models/trained_model.mat');
+end
+
+function loadSampleCallback(~, ~)
+    data = load(fullfile('data', 'S1_A1_E1.mat'));
+    X = data.emg;
+    idx = randi(size(X, 1));
+    sample = X(idx, :);
+    assignin('base', 'currentSample', sample);
+    msgbox('Random EMG sample loaded into memory.');
+end
+
+function predictCallback(~, ~)
+    sample = evalin('base', 'currentSample');
+    if isempty(sample)
+        errordlg('Please load a sample first.');
+        return;
     end
+
+    prediction = predict_gesture(sample);
+
+    % Update GUI
+    label = findall(gcf, 'Tag', 'predictionLabel');
+    set(label, 'String', ['Prediction: ', num2str(prediction)]);
 end
