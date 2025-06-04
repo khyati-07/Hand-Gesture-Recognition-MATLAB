244e949e49020e6aabc885a72c467a3c41067a67
khyati-07 <141446282+khyati-07@users.noreply.github.com>
2025-06-04 12:58:00 +0530

    Real time simulate

--- Hand-Gesture-MATLAB/functions/predict_gesture.m
+++ Hand-Gesture-MATLAB/functions/predict_gesture.m
@@ -1,10 +1,5 @@
-function prediction = predict_gesture(sample)
-    if ~isfile('models/trained_model.mat')
-        error('Trained model not found. Run training first.');
-    end
-
+function label = predict_gesture(emg_window)
     load('models/trained_model.mat', 'model');
-
-    feat = extract_features(sample);
-    prediction = predict(model, feat);
+    feats = extract_features(emg_window);
+    label = predict(model, feats);
 end
